87f1cc3a095a22a4e5582d31112dcb39e39ad968
Steph Prince <40640337+stephprince@users.noreply.github.com>
2024-03-18 16:43:36 -0700

    add electrode table tests

--- tests/aq-nwb_test.cpp
+++ tests/aq-nwb_test.cpp
@@ -5,6 +5,7 @@
 
 #include "BaseIO.hpp"
 #include "HDF5IO.hpp"
+#include "NWBDataTypes.hpp"
 #include "NWBFile.hpp"
 
 namespace fs = std::filesystem;
@@ -27,7 +28,7 @@ std::string getTestFilePath(std::string filename)
   return filepath.u8string();
 }
 
-TEST_CASE("write_attributes", "[hdf5io]")
+TEST_CASE("writeAttributes", "[hdf5io]")
 {
   // create and open file
   std::string filename = getTestFilePath("test_attributes.h5");
@@ -54,7 +55,7 @@ TEST_CASE("write_attributes", "[hdf5io]")
         AQNWBIO::BaseDataType::I32, &data, "/data", "array", dataSize);
   }
 
-  // integer array
+  // string array
   SECTION("str_array")
   {
     const std::vector<std::string> data = {"col1", "col2", "col3"};
@@ -62,15 +63,81 @@ TEST_CASE("write_attributes", "[hdf5io]")
     hdf5io.setAttribute(data, "/data", "string_array");
   }
 
+  // soft link
+  SECTION("link")
+  {
+    std::vector<std::string> data;
+    hdf5io.createLink("/data/link", "linked_data");
+  }
+
   // close file
   hdf5io.close();
 }
 
-TEST_CASE("generate_nwbfile", "[nwb]")
+TEST_CASE("saveNWBFile", "[nwb]")
 {
   std::string filename = getTestFilePath("test_nwb_file.h5");
 
   NWBFile nwbfile("123", std::make_unique<HDF5IO>(filename));
   nwbfile.initialize();
   nwbfile.finalize();
+}
+
+TEST_CASE("startRecording", "[nwb]")
+{
+  std::string filename = getTestFilePath("test_recording.h5");
+
+  NWBFile nwbfile("123", std::make_unique<HDF5IO>(filename));
+  nwbfile.initialize();
+  bool result = nwbfile.startRecording();
+  nwbfile.finalize();
+
+  REQUIRE(result == true);
+}
+
+TEST_CASE("ElectrodeTable", "[datatypes]")
+{
+  std::string path = "/electrodes/";
+  SECTION("initialize with example data")
+  {
+    std::string filename = getTestFilePath("electrodeTable.h5");
+    std::shared_ptr<BaseIO> io = std::make_unique<HDF5IO>(filename);
+    io->open();
+    ElectrodeTable electrodeTable(path, io);
+    io->createGroup("array1");
+
+    std::vector<int> channels = {1, 2, 3};
+    electrodeTable.channels = channels;
+    electrodeTable.groupPath = "array1";
+    electrodeTable.electrodeDataset->dataset =
+        std::unique_ptr<BaseRecordingData>(
+            io->createDataSet(BaseDataType::I32, 1, 1, path + "id"));
+    electrodeTable.locationsDataset->dataset =
+        std::unique_ptr<BaseRecordingData>(
+            io->createDataSet(BaseDataType::STR(250), 0, 1, path + "location"));
+    electrodeTable.initialize();
+
+    // Check if the groupReferences, groupNames, electrodeNumbers, and
+    // locationNames vectors are populated correctly
+    REQUIRE(electrodeTable.locationNames
+            == std::vector<std::string> {"unknown", "unknown", "unknown"});
+    REQUIRE(electrodeTable.electrodeNumbers == std::vector<int> {1, 2, 3});
+    REQUIRE(electrodeTable.getColNames()
+            == std::vector<std::string> {"group", "group_name", "location"});
+  }
+
+  SECTION("initialize without data")
+  {
+    std::string filename = getTestFilePath("electrodeTableNoData.h5");
+    std::shared_ptr<BaseIO> io = std::make_unique<HDF5IO>(filename);
+    io->open();
+    ElectrodeTable electrodeTable(path, io);
+    electrodeTable.initialize();
+
+    // Check if the groupReferences, groupNames, electrodeNumbers, and
+    // locationNames vectors are empty
+    REQUIRE(electrodeTable.groupReferences.empty());
+    REQUIRE(electrodeTable.groupNames.empty());
+    REQUIRE(electrodeTable.electrodeNumbers.empty());
+  }
 }
