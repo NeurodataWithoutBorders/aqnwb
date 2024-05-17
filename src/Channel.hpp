937b5fe702ee63a71b89bf1d0f610894c83f2629
Steph Prince <40640337+stephprince@users.noreply.github.com>
2024-05-17 11:34:01 -0700

    add getters for channel class

--- src/Channel.hpp
+++ src/Channel.hpp
@@ -19,11 +19,13 @@ public:
           int globalIndex,
           float conversion = 1e6,
           float samplingRate = 30000,
+          float bitVolts = 0.195,
           std::vector<float> position = {0, 0, 0})
       : name(name)
       , groupName(groupName)
       , conversion(conversion)
       , samplingRate(samplingRate)
+      , bitVolts(bitVolts)
       , localIndex(localIndex)
       , globalIndex(globalIndex)
       , position(position)
@@ -36,6 +38,24 @@ public:
   ~Channel() {}
 
   /**
+   * @brief Getter for conversion factor
+   * @return The conversion value.
+   */
+  float getConversion() const { return conversion; }
+
+  /**
+   * @brief Getter for samplingRate
+   * @return The samplingRate value.
+   */
+  float getSamplingRate() const { return samplingRate; }
+
+  /**
+   * @brief Getter for bitVolts
+   * @return The bitVolts value.
+   */
+  float getBitVolts() const { return bitVolts; }
+
+  /**
    * @brief Name of the channel.
    */
   std::string name;
@@ -44,16 +64,6 @@ public:
    * @brief Name of the array group the channel belongs to.
    */
   std::string groupName;
-
-  /**
-   * @brief Conversion factor.
-   */
-  float conversion;
-
-  /**
-   * @brief Sampling rate of the channel.
-   */
-  float samplingRate;
 
   /**
    * @brief Index of channel within the recording array.
@@ -71,5 +81,19 @@ public:
   std::vector<float> position;
 
 private:
+  /**
+   * @brief Conversion factor.
+   */
+  float conversion;
+
+  /**
+   * @brief Sampling rate of the channel.
+   */
+  float samplingRate;
+
+  /**
+   * @brief floating point value of microvolts per bit
+   */
+  float bitVolts;
 };
 }  // namespace AQNWB
