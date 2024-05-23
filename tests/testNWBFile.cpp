9938a71278dbe0049a8386b1daeed1d31e9d7cdc
Steph Prince <40640337+stephprince@users.noreply.github.com>
2024-05-23 10:54:53 -0700

    update startRecording test to not have empty timeseries

--- tests/testNWBFile.cpp
+++ tests/testNWBFile.cpp
@@ -31,6 +31,15 @@ TEST_CASE("startRecording", "[nwb]")
   // start recording
   std::vector<Types::ChannelGroup> mockArrays = getMockChannelArrays();
   Status result = nwbfile.startRecording(mockArrays);
+
+  // write timeseries data
+  std::vector<float> mockData = {1.0, 2.0, 3.0, 4.0, 5.0};
+  std::vector<float> mockTimestamps = {0.1, 0.2, 0.3, 0.4, 0.5};
+  nwbfile.writeTimeseriesData(0, 0, 5, BaseDataType::F32, mockData.data());
+  nwbfile.writeTimeseriesTimestamps(0, 5, BaseDataType::F32, mockTimestamps.data());
+  nwbfile.writeTimeseriesData(1, 0, 5, BaseDataType::F32, mockData.data());
+  nwbfile.writeTimeseriesTimestamps(1, 5, BaseDataType::F32, mockTimestamps.data());
+
   nwbfile.finalize();
 
   REQUIRE(result == Status::Success);
