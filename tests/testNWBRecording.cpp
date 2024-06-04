d17f078fac223b102b692726a41d8d45bdc84c27
Steph Prince <40640337+stephprince@users.noreply.github.com>
2024-06-04 12:01:54 -0700

    add data and timestamps content check to tests

--- tests/testNWBRecording.cpp
+++ tests/testNWBRecording.cpp
@@ -1,5 +1,7 @@
 
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_all.hpp>
+#include <H5Cpp.h>
 
 #include "BaseIO.hpp"
 #include "Channel.hpp"
@@ -37,7 +39,7 @@ TEST_CASE("writeContinuousData", "[recording]")
 
     // open files
     NWB::NWBRecording nwbRecording;
-    nwbRecording.openFiles(path, "Recording", 1, mockRecordingArrays);
+    nwbRecording.openFile(path, "Recording", 1, mockRecordingArrays);
 
     // run recording
     bool isRecording = true;
@@ -69,11 +71,43 @@ TEST_CASE("writeContinuousData", "[recording]")
         isRecording = false;
       }
     }
-  }
+    nwbRecording.closeFile();
 
-  SECTION("check contents of data and timestamps", "[recording]")
-  {
-    // TODO
+    // check contents of data
+    std::string dataPath = "/acquisition/array1/data";
+    std::unique_ptr<H5::H5File> file = std::make_unique<H5::H5File>(path + "Recording1.nwb", H5F_ACC_RDONLY);
+    std::unique_ptr<H5::DataSet> dataset = std::make_unique<H5::DataSet>(file->openDataSet(dataPath));
+    SizeType numChannelsToRead = numChannels / 2;
+
+    float* buffer = new float[numSamples*numChannelsToRead];
+
+    H5::DataSpace fSpace = dataset->getSpace();
+    hsize_t dims[1] = {numSamples * numChannelsToRead};
+    H5::DataSpace mSpace(1, dims);
+    dataset->read(buffer, H5::PredType::NATIVE_FLOAT, mSpace, fSpace);   
+
+    std::vector<std::vector<float>> dataOut(numChannelsToRead, std::vector<float>(numSamples));
+    for(SizeType i = 0; i < numChannelsToRead; ++i) {
+        for(SizeType j = 0; j < numSamples; ++j) {
+            dataOut[i][j] = buffer[j*numChannelsToRead + i] * (32767.0f * 0.000002f);
+        }
+    }
+    delete[] buffer;
+    REQUIRE_THAT(dataOut[0], Catch::Matchers::Approx(mockData[0]).margin(1));
+    REQUIRE_THAT(dataOut[1], Catch::Matchers::Approx(mockData[1]).margin(1));
+
+    // check contents of timestamps
+    std::string timestampsPath = "/acquisition/array1/timestamps";
+    std::unique_ptr<H5::DataSet> tsDataset = std::make_unique<H5::DataSet>(file->openDataSet(timestampsPath));
+    double* tsBuffer = new double[numSamples];
+
+    H5::DataSpace tsfSpace = tsDataset->getSpace();
+    tsDataset->read(tsBuffer, H5::PredType::NATIVE_DOUBLE, tsfSpace, tsfSpace);   
+
+    std::vector<double> timestampsOut(tsBuffer, tsBuffer + numSamples);
+    delete[] tsBuffer;
+    double tolerance = 1e-9;
+    REQUIRE_THAT(timestampsOut, Catch::Matchers::Approx(mockTimestamps).margin(tolerance));
   }
 
   SECTION("test if more samples than buffer size", "[recording]")
