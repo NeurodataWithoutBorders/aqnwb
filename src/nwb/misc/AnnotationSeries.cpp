13515adf4e73c857bafeb33f9ed75971d08d746b
Steph Prince <40640337+stephprince@users.noreply.github.com>
2024-08-04 22:01:21 -0700

    add annotation series

---
+++ src/nwb/misc/AnnotationSeries.cpp
@@ -0,0 +1,27 @@
+#include "nwb/misc/AnnotationSeries.hpp"
+
+using namespace AQNWB::NWB;
+
+// AnnotationSeries
+
+/** Constructor */
+AnnotationSeries::AnnotationSeries(const std::string& path,
+                       std::shared_ptr<BaseIO> io,
+                       const BaseDataType& dataType,
+                       const std::string& description,
+                       const std::string& comments,
+                       const SizeArray& dsetSize,
+                       const SizeArray& chunkSize)
+    : TimeSeries(path,
+                 io,
+                 dataType,
+                 "n/a",  // unit is 'n/a' for AnnotationSeries
+                 description,
+                 comments,
+                 dsetSize,
+                 chunkSize)
+{
+}
+
+/** Destructor */
+AnnotationSeries::~AnnotationSeries() {}
