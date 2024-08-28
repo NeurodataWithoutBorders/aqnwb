5b54b9c5f34fd2672726cdf518832f3a2cf532fd
Steph Prince <40640337+stephprince@users.noreply.github.com>
2024-08-28 09:42:39 -0700

    update package name and clean up

--- include/aqnwb/aqnwb.hpp
+++ include/aqnwb/aqnwb.hpp
@@ -3,25 +3,5 @@
 #include <string>
 
 #include "aqnwb/aqnwb_export.hpp"
-
-/**
- * @brief Reports the name of the library
- *
- * Please see the note above for considerations when creating shared libraries.
- */
-class AQNWB_EXPORT exported_class
-{
-public:
-  /**
-   * @brief Initializes the name field to the name of the project
-   */
-  exported_class();
-
-  /**
-   * @brief Returns a non-owning pointer to the string stored in this class
-   */
-  auto name() const -> char const*;
-
-private:
-  std::string m_name;
-};
+#include "aqnwb/nwb/NWBRecording.hpp"
+#include "aqnwb/nwb/NWBFile.hpp"
