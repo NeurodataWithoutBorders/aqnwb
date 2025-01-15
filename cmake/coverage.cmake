4f5cc9979f162301d2579ac0904b54394286959c
Steph Prince <40640337+stephprince@users.noreply.github.com>
2025-01-15 11:10:16 -0800

    exclude tests from coverage trace

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -7,7 +7,8 @@ set(
     lcov -c -q
     -o "${PROJECT_BINARY_DIR}/coverage.info"
     -d "${PROJECT_BINARY_DIR}"
-    --include "${PROJECT_SOURCE_DIR}/src/*"
+    --include "${PROJECT_SOURCE_DIR}/*"
+    --exclude "*/tests/*"
     CACHE STRING
     "; separated command to generate a trace for the 'coverage' target"
 )
