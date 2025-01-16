12ab834552b72a083b2dfaeafc5dcf0cf758e7e8
Steph Prince <40640337+stephprince@users.noreply.github.com>
2025-01-15 16:38:08 -0800

    debug coverage dir

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -7,7 +7,6 @@ set(
     lcov -c --verbose
     -o "${PROJECT_BINARY_DIR}/coverage.info"
     -d "${PROJECT_BINARY_DIR}"
-    -d "${PROJECT_SOURCE_DIR}/src/*"
     --no-external
     CACHE STRING
     "; separated command to generate a trace for the 'coverage' target"
