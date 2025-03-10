cbcecb27321718ec595abbe7dd327b0b8fdace72
Oliver Ruebel <oruebel@users.noreply.github.com>
2025-03-10 16:24:34 -0700

    Apply suggestions from code review
    Co-authored-by: Steph Prince <40640337+stephprince@users.noreply.github.com>

--- src/nwb/hdmf/table/ElementIdentifiers.cpp
+++ src/nwb/hdmf/table/ElementIdentifiers.cpp
@@ -1,12 +1,9 @@
 #include "nwb/hdmf/table/ElementIdentifiers.hpp"
-// #include "nwb/hdmf/table/VectorData.hpp"
 
 using namespace AQNWB::NWB;
 
 // Initialize the static registered_ member to trigger registration
 REGISTER_SUBCLASS_IMPL(ElementIdentifiers)
-// REGISTER_SUBCLASS_IMPL(Data)
-// REGISTER_SUBCLASS_IMPL(VectorData)
 
 /** Constructor */
 ElementIdentifiers::ElementIdentifiers(const std::string& path,
