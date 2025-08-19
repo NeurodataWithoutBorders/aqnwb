6335c6346911e8ff2a9a47e3965e4123a8166465
Steph Prince <40640337+stephprince@users.noreply.github.com>
2025-08-19 13:57:20 -0700

    fix formatting

--- src/nwb/RegisteredType.hpp
+++ src/nwb/RegisteredType.hpp
@@ -450,7 +450,7 @@ protected:
    * description \
    */ \
   template<typename VTYPE = default_type> \
-  inline std::unique_ptr<AQNWB::IO::ReadDataWrapper<AttributeField, VTYPE>> 
+  inline std::unique_ptr<AQNWB::IO::ReadDataWrapper<AttributeField, VTYPE>> \
   name() const \
   { \
     return std::make_unique< \
