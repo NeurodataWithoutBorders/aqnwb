9516ba6caf8edde7ec806d13b77bf3161e87d847
Oliver Ruebel <oruebel@lbl.gov>
2026-07-02 08:07:15 -0700

    fix linter

--- src/nwb/hdmf/table/MeaningsTable.cpp
+++ src/nwb/hdmf/table/MeaningsTable.cpp
@@ -35,7 +35,8 @@ Status MeaningsTable::initialize(const VectorData& targetVectorData,
 
   // Call parent initialize method. This initializes the DynamicTable and
   // creates the description attribute. Column names are written by
-  // DynamicTable::finalize(). ElementIdentifiers ids are set in the constructor.
+  // DynamicTable::finalize(). ElementIdentifiers ids are set in the
+  // constructor.
   Status parentInitStatus = DynamicTable::initialize(description);
   initStatus = initStatus && parentInitStatus;
 
