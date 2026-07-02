8046b990fc7ab26e591ba62e31d2acc33681f517
Oliver Ruebel <oruebel@lbl.gov>
2026-07-02 05:03:41 -0700

    Add base implemention of the MeansingTable type

---
+++ src/nwb/hdmf/table/MeaningsTable.cpp
@@ -0,0 +1,71 @@
+#include "MeaningsTable.hpp"
+
+#include "Utils.hpp"
+// Includes for referenced types
+#include "nwb/hdmf/table/VectorData.hpp"
+
+using namespace AQNWB::NWB;
+using namespace AQNWB::IO;
+
+// Initialize the static registered_ member to trigger registration
+REGISTER_SUBCLASS_IMPL(MeaningsTable)
+
+// Constructor
+MeaningsTable::MeaningsTable(const std::string& path,
+                             std::shared_ptr<AQNWB::IO::BaseIO> io)
+    : DynamicTable(path, io)
+{
+}
+
+// Initialize the object
+Status MeaningsTable::initialize(const VectorData& targetVectorData,
+                                 const AQNWB::IO::BaseDataType& valueDataType,
+                                 const std::string& description,
+                                 const SizeType rowChunkSize)
+{
+  Status initStatus = Status::Success;
+
+  // Retrieve the IO object
+  auto ioPtr = getIO();
+  if (ioPtr == nullptr) {
+    std::cerr << "IO object has been deleted. Can't initialize MeaningsTable: "
+              << m_path << std::endl;
+    return Status::Failure;
+  }
+
+  // Call parent initialize method. This initializes the DynamicTable and
+  // creates the description and colnames attributes. ElementIdentifiers ids are
+  // set in the constructor.
+  Status parentInitStatus = DynamicTable::initialize(description);
+  initStatus = initStatus && parentInitStatus;
+  ;
+
+  // Initialize value dataset
+  auto valuePath = AQNWB::mergePaths(m_path, "value");
+  IO::ArrayDataSetConfig valueConfig(
+      valueDataType, SizeArray {0}, SizeArray {rowChunkSize});
+  auto valuesColmn = VectorData::create(valuePath, ioPtr);
+  Status valueStatus = valuesColmn->initialize(
+      valueConfig, "The value of a row in the linked VectorData object.");
+  Status addValueColumnStatus = addColumn(valuesColmn);
+
+  // Initialize meaning dataset
+  auto meaningPath = AQNWB::mergePaths(m_path, "meaning");
+  IO::ArrayDataSetConfig meaningConfig(
+      IO::BaseDataType::V_STR, SizeArray {0}, SizeArray {rowChunkSize});
+  auto meaningColumn = VectorDataTyped<std::string>::create(meaningPath, ioPtr);
+  Status meaningStatus = meaningColumn->initialize(
+      meaningConfig,
+      "The meaning of the value in the linked VectorData object.");
+  Status addMeaningColumnStatus = addColumn(meaningColumn);
+
+  // Set the target VectorData reference
+  auto targetPath = AQNWB::mergePaths(m_path, "target");
+  Status targetStatus =
+      ioPtr->createLink(targetVectorData.getPath(), targetPath);
+
+  // Update the overall status and return the final status
+  initStatus = initStatus && valueStatus && addValueColumnStatus
+      && meaningStatus && addMeaningColumnStatus && targetStatus;
+  return initStatus;
+}
---
+++ src/nwb/hdmf/table/MeaningsTable.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+// Common STL includes
+#include <memory>
+#include <optional>
+#include <string>
+#include <vector>
+// Base AqNWB includes for IO and RegisteredType
+#include "io/BaseIO.hpp"
+#include "io/ReadIO.hpp"
+#include "nwb/RegisteredType.hpp"
+// Include for parent type
+#include "nwb/hdmf/table/DynamicTable.hpp"
+// Forward declarations for referenced types
+namespace AQNWB::NWB
+{
+class ElementIdentifiers;
+class VectorData;
+}  // namespace AQNWB::NWB
+// Include for the namespace schema header
+#include "spec/hdmf_common.hpp"
+
+namespace AQNWB::NWB
+{
+
+/**
+ * @brief A table to store information about the meanings of values in a linked
+ * VectorData object. All possible values of the linked VectorData object should
+ * be present in the 'value' column of this table, even if the value is not
+ * observed in the data. Additional columns may be added to store additional
+ * metadata about each value. The name of the MeaningsTable should correspond to
+ * the name of the linked VectorData object with a "_meanings" suffix. e.g., if
+ * the linked VectorData object is named "stimulus_type", the corresponding
+ * MeaningsTable should be named "stimulus_type_meanings".
+ */
+class MeaningsTable : public AQNWB::NWB::DynamicTable
+{
+public:
+  /**
+   * @brief Constructor
+   * @param path Path to the object in the file
+   * @param io IO object for reading/writing
+   */
+  MeaningsTable(const std::string& path, std::shared_ptr<AQNWB::IO::BaseIO> io);
+
+  /**
+   * @brief Virtual destructor.
+   */
+  virtual ~MeaningsTable() override {}
+
+  /**
+   * @brief Initialize the object
+   * @param targetVectorData The VectorData object that this MeaningsTable is
+   * associated with.
+   * @param valueDataType The data type of the 'value' column in the
+   * MeaningsTable.
+   * @param description Description of the table (optional)
+   * @param rowChunkSize The chunk size for the rows of the table (optional,
+   * default: 100)
+   * @return Status::Success if successful, otherwise Status::Failure.
+   */
+  Status initialize(const VectorData& targetVectorData,
+                    const AQNWB::IO::BaseDataType& valueDataType,
+                    const std::string& description =
+                        "A table to store information about the meanings of "
+                        "values in a linked VectorData object.",
+                    const SizeType rowChunkSize = 100);
+
+  DEFINE_REGISTERED_FIELD(readValueColumn,
+                          AQNWB::NWB::VectorData,
+                          "value",
+                          "The value of a row in the linked VectorData object.")
+
+  DEFINE_REGISTERED_FIELD(
+      readMeaningColumn,
+      AQNWB::NWB::VectorDataTyped<std::string>,
+      "meaning",
+      "The meaning of the value in the linked VectorData object.")
+
+  DEFINE_REFERENCED_REGISTERED_FIELD(
+      readTarget,
+      AQNWB::NWB::VectorData,
+      "target",
+      "The VectorData object for which this table provides meanings.")
+
+  REGISTER_SUBCLASS(MeaningsTable,
+                    DynamicTable,
+                    AQNWB::SPEC::HDMF_COMMON::namespaceName)
+};
+
+}  // namespace AQNWB::NWB
