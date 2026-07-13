571ef5b243806143d59174aab09cfbb28d973148
Oliver Ruebel <oruebel@lbl.gov>
2026-07-13 15:34:25 -0700

    Attempt to fix demo workflow

--- cmake/install-config.cmake
+++ cmake/install-config.cmake
@@ -3,3 +3,12 @@ include(CMakeFindDependencyMacro)
 find_dependency(HDF5 COMPONENTS CXX)
 
 include("${CMAKE_CURRENT_LIST_DIR}/aqnwbTargets.cmake")
+
+# The aqnwb_aqnwb target only propagates HDF5 include directories via
+# $<BUILD_INTERFACE:...> (see the top-level CMakeLists.txt), so they are not
+# part of the installed target's INTERFACE_INCLUDE_DIRECTORIES. Explicitly add
+# the HDF5 include directories found above to the imported aqnwb::aqnwb target
+# so that downstream consumers (e.g. via find_package(aqnwb)) can locate HDF5
+# C/C++ headers regardless of where HDF5 is installed on the system.
+target_include_directories(aqnwb::aqnwb SYSTEM INTERFACE ${HDF5_INCLUDE_DIRS})
+
